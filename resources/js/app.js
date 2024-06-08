8a69ea970a85d4aa4bf6b93ddcd5534f62bd151a
saify <sarfaraz.symits@gmail.com>
2024-06-08 23:12:06 +0500

    hide sidebar scrollbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import persist from '@alpinejs/persist'
+import Swiper from 'swiper';
+import 'swiper/css';
+
 window.Alpine = Alpine;
 
 Alpine.plugin(persist)
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,3 @@
 <x-base-layout>
-    <x-breadcrumb title="Chart" path="dashboard"/>
-    <x-alert title="sss" type="error"/>
+
 </x-base-layout>
